1159f3c239dfcf4ec5de5d3ab5e528d507a5d759
heyuantao <he_yuan_tao@163.com>
2020-03-18 09:01:25 +0800

    field

--- MAIN/models.py
+++ MAIN/models.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 # Create your models here.
 class FileModel(models.Model):
     filename = models.CharField(max_length=300)
-    filesize = models.IntegerField(default=0)
+    filesize = models.BigIntegerField(default=0)
     key = models.CharField(max_length=300)
     uploaddate = models.DateTimeField(auto_now=True)
     browserable = models.BooleanField(default=True)        #设置文件是否对普通用户可浏览
