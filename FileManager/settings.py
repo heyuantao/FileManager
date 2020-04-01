be730655cecceb13d9c65fb16910ac0e0eb8a653
heyuantao <he_yuan_tao@163.com>
2020-04-01 20:31:19 +0800

    debug false

--- FileManager/settings.py
+++ FileManager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z=jqj%7@7a7!!wbgu2js3hz*fgvm2$!!)z$e*#aph3(bl&bc-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
