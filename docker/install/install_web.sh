3ead24b5a58d75257bbc3bf1a62f97e938d7e9d2
heyuantao <he_yuan_tao@163.com>
2020-05-07 10:08:24 +0800

    con

--- docker/install/install_web.sh
+++ docker/install/install_web.sh
@@ -1,27 +1,7 @@
-echo "Remove the node_modules !"
-rm -Rf /app/FileManager/media/manager/node_modules
-rm -Rf /app/FileManager/media/guest/node_modules
-
-
-#echo "Set Apt Source List !"
-#sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
-#echo "Asia/Shanghai" > /etc/timezone
-#dpkg-reconfigure -f noninteractive tzdata
-
-#echo "Change Lang !"
-#apt-get update
-#apt-get install -y locales
-#locale-gen zh_CN
-#locale-gen zh_CN.utf8
-#export LANG=zh_CN.UTF-8
-#export LC_ALL=zh_CN.UTF-8
-#export LANGUAGE=zh_CN.UTF-8
-
-
-echo "Install Apt Package !"
-apt-get install -y nginx supervisor
-apt-get install -y python3 python3-pip python3-dev libmysqlclient-dev #python-mysqldb
-apt-get install -y libssl-dev
+#echo "Install Apt Package !"
+#apt-get install -y nginx supervisor
+#apt-get install -y python3 python3-pip python3-dev libmysqlclient-dev #python-mysqldb
+#apt-get install -y libssl-dev
 
 
 echo "Install virtualenv and requirements !"
